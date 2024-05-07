b0d7f02c9ca013c5ca422de3ff9712e51e64f0be
JoaoNeto4 <joaoquatlo@hotmail.com>
2024-05-07 20:43:47 -0300

    session management

--- src/main/java/com/mballem/curso/security/config/SecurityConfig.java
+++ src/main/java/com/mballem/curso/security/config/SecurityConfig.java
@@ -1,12 +1,19 @@
 package com.mballem.curso.security.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.session.SessionRegistry;
+import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+import org.springframework.security.web.session.HttpSessionEventPublisher;
 
 import com.mballem.curso.security.domain.PerfilTipo;
 import com.mballem.curso.security.service.UsuarioService;
@@ -27,7 +34,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 		
 		http.authorizeRequests()
 			.antMatchers("/webjars/**", "/css/**", "/image/**", "/js/**").permitAll()
-			.antMatchers("/", "/home").permitAll()
+			.antMatchers("/", "/home", "/expired").permitAll()
 			.antMatchers("/u/novo/cadastro", "/u/cadastro/realizado", "/u/cadastro/paciente/salvar").permitAll()
 			.antMatchers("/u/confirmacao/cadastro").permitAll()
 			.antMatchers("/u/p/**").permitAll()
@@ -66,6 +73,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 				.accessDeniedPage("/acesso-negado")
 			.and()
 				.rememberMe();/* remember-me for login page*/
+		
+		
+		http.sessionManagement()//gerenciamento de sessão
+				.maximumSessions(1)//max sessions
+				.expiredUrl("/expired")
+				.maxSessionsPreventsLogin(true)//permite login em um segundo dispositivo?
+				.sessionRegistry(sessionRegistry());
+		
+		//or
+		/* 
+		 http.sessionManagement()
+		 		.sessionFixation().newSession()
+		 		.sessionAuthenticationStrategy(sessionAuthenticationStrategy());//set false -> .maxSessionsPreventsLogin(false)
+		*/
+				
 	}
 
 	@Override
@@ -77,6 +99,34 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 		auth.userDetailsService(service).passwordEncoder(new BCryptPasswordEncoder());
 	}
 	
+	
+	
+	
+	
+	
+	
+	@Bean
+	public SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+		return new RegisterSessionAuthenticationStrategy(sessionRegistry());
+	}
+	
+	
+	
+	@Bean
+	public SessionRegistry sessionRegistry() {
+		/*
+		 * responsavel por passar configuração de sessao a cada login utilizado na sessão
+		 */
+		return new SessionRegistryImpl();
+	}
+	
+	@Bean
+	public ServletListenerRegistrationBean<?> servletListenerRegistrationBean() {
+		/*
+		 * registra os logins no servlet da applicação
+		 */
+		return new ServletListenerRegistrationBean<>(new HttpSessionEventPublisher());
+	}
 	
 
 	
--- src/main/java/com/mballem/curso/security/web/controller/HomeController.java
+++ src/main/java/com/mballem/curso/security/web/controller/HomeController.java
@@ -1,7 +1,10 @@
 package com.mballem.curso.security.web.controller;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import org.springframework.security.web.authentication.session.SessionAuthenticationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,7 +25,18 @@ public class HomeController {
 	
 	// login error
 	@GetMapping({"/login-error"})
-	public String loginError(ModelMap model) {
+	public String loginError(ModelMap model, HttpServletRequest resp) {
+		HttpSession session = resp.getSession();
+		String lastException = String.valueOf(session.getAttribute("SPRING_SECURITY_LAST_EXCEPTION"));
+		if (lastException.contains(SessionAuthenticationException.class.getName())) {
+			model.addAttribute("alerta", "erro");
+			model.addAttribute("titulo", "Acesso rrecusado!");
+			model.addAttribute("texto", "Você ja esta logado em outro dispositivo.");
+			model.addAttribute("subTexto", "Faça logout ou espere a sessão expirar.");
+			
+			return "login";
+		}
+		
 		model.addAttribute("alerta", "erro");
 		model.addAttribute("titulo", "Credenciais invãlidas!");
 		model.addAttribute("texto", "Login ou senha incorretos, tente novamente.");
@@ -30,6 +44,25 @@ public class HomeController {
 		
 		return "login";
 	}
+	
+	
+		// login error
+		@GetMapping({"/expired"})
+		public String sessaoExpirada(ModelMap model) {
+			/* used if SecurityConfig.class:
+			 http.sessionManagement()
+			 		.sessionFixation().newSession()
+			 		.sessionAuthenticationStrategy(sessionAuthenticationStrategy());//set false -> .maxSessionsPreventsLogin(false)
+			*/
+			
+			model.addAttribute("alerta", "erro");
+			model.addAttribute("titulo", "Acesso recusado");
+			model.addAttribute("texto", "Sua sessão expirou.");
+			model.addAttribute("subTexto", "Você logou em outro dispositivo.");
+			
+			return "login";
+		}
+		
 	
 	// access denied
 	@GetMapping({"/acesso-negado"})
